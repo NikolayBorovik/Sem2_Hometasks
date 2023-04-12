5bebb60dade13c64182dc6546cf7e46dee9a62e7
Nikolay Borovik <nik.Borovik@bk.ru>
2023-04-12 14:16:33 +1000

    task 10 done

--- 10.py
+++ 10.py
@@ -3,3 +3,17 @@
 # монеток, которые нужно перевернуть, чтобы все монетки были
 # повернуты вверх одной и той же стороной. Выведите минимальное
 # количество монет, которые нужно перевернуть.
+
+coins = int(input('Enter total number of coins: '))
+heads = 0
+trails = 0
+for i in range(coins):
+    side = input(f'Enter side of the coin{i+1} (head or trail): ')
+    if side == 'trail':
+        trails +=1
+    elif side == 'head':
+        heads += 1
+if trails<heads:
+    print(f"Turn trails upside down {trails} times")
+else:
+    print(f"Turn heads upside down {heads} times")
